cf60818a91afe4a42f11e11e212b382662ecbfb4
unknown <MyCraftablRomnce@gmail.com>
2020-01-28 15:38:16 -0800

    Completed the second problem

--- script2.js
+++ script2.js
@@ -12,6 +12,11 @@
 
 let i = 1;
 while(i<=100) {
-    console.log(i);
+     let output = "";
+     if (i % 3 == 0) output += "Fizz";
+     if (i % 5 == 0) output += "Buzz";
+     console.log(output || i);
      i++;
+     
 }
+
